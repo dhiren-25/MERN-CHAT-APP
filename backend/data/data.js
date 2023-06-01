c2e4255b91775f576635a2a3d4bf08bdb2bd2027
dhiren-25 <chaudharidhiren253@gmail.com>
2023-06-02 01:17:03 +0530

    first

---
+++ backend/data/data.js
@@ -0,0 +1,36 @@
+const chats = [
+    {
+        isGroupChat: false,
+        users:[
+            {
+                name: "Dhiren Chaudhari",
+                email: "dhiren@gmail.com",
+            },
+            {
+                name: "Himanshu Amliyar",
+                email: "himanshu@gmail.com",
+            },
+
+        ],
+        _id: "617a077e18c2546bc7c4ddd4",
+        chatName: "Dhiren Chaudhari",
+    },
+    {
+        isGroupChat: false,
+        users:[
+            {
+                name: "Sid Chaudhari",
+                email: "Sid@gmail.com",
+            },
+            {
+                name: "Rid Patel   ",
+                email: "rid@gmail.com",
+            },
+
+        ],
+        _id: "645a54e18c2546bc7c4ddd4",
+        chatName: "Himanshu Amaliyar",
+    }
+];
+module.exports={chats};
+
